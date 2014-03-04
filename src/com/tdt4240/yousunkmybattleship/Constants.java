17880c70108bcaed2c1870282fa1ed75a4337403
larsmh <lars.mh23@gmail.com>
2014-03-04 13:59:16 +0100

    pictures added

--- src/com/tdt4240/yousunkmybattleship/Constants.java
+++ src/com/tdt4240/yousunkmybattleship/Constants.java
@@ -4,6 +4,7 @@ import sheep.game.Game;
 
 public class Constants {
 	public static Game game;
+	public static Player p1, p2;
 	
 	/* Window sizes */
 	public static int WINDOW_WIDTH  = 0;
@@ -14,4 +15,11 @@ public class Constants {
 	public static int GRID_HEIGHT = 10;
 	
 	public static int NUMBER_SHIPS = 5;
+	
+	public static Player getOther(Player p){
+		if(p==p1)
+			return p2;
+		else
+			return p1;
+	}
 }
