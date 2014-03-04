1b004039d494455ae741230bc745a8da128ca77f
Chloebrz <chloe.brouzes@hotmail.fr>
2014-03-04 12:48:08 +0100

    add constants

--- src/com/tdt4240/yousunkmybattleship/Constants.java
+++ src/com/tdt4240/yousunkmybattleship/Constants.java
@@ -4,6 +4,13 @@ import sheep.game.Game;
 
 public class Constants {
 	public static Game game;
+	
+	/* Window sizes */
 	public static int WINDOW_WIDTH  = 0;
 	public static int WINDOW_HEIGHT = 0;
+	
+	/* Grid properties */
+	public static int GRID_WIDTH = 10;
+	public static int GRID_HEIGHT = 10;
+	
 }
