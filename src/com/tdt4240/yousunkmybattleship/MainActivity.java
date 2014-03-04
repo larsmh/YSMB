fa6c127696502c1ebccbd8d888ed564c3bf99f68
larsmh <lars.mh23@gmail.com>
2014-03-04 11:15:51 +0100

    MainMenuState class added

--- src/com/tdt4240/yousunkmybattleship/MainActivity.java
+++ src/com/tdt4240/yousunkmybattleship/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tdt4240.yousunkmybattleship;
 
+import sheep.game.Game;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,7 +10,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Game game = new Game(this, null);
+        game.pushState(new MainMenuState());
+        setContentView(game);
     }
 
 
