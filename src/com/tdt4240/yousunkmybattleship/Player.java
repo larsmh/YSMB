efce94b0eb35151210ec262e28b398a58dcc244c
Chloebrz <chloe.brouzes@hotmail.fr>
2014-03-04 13:32:28 +0100

    Player

--- src/com/tdt4240/yousunkmybattleship/Player.java
+++ src/com/tdt4240/yousunkmybattleship/Player.java
@@ -2,10 +2,15 @@ package com.tdt4240.yousunkmybattleship;
 
 public class Player {
 	private String name;
+	
+	// The number of non sunk ships of the player
+	private int shipsRemaining;
+	
 	private PlayerState playerState;
 	
 	public Player(String name) {
 		this.name = name;
+		shipsRemaining = Constants.NUMBER_SHIPS;
 	}
 	
 	public String getName() {
@@ -18,5 +23,13 @@ public class Player {
 	
 	public PlayerState getPlayerState() {
 		return playerState;
+	}
+	
+	public void shipSunk() {
+		shipsRemaining -= 1;
+		
+		if(shipsRemaining == 0) {
+			// game over
+		}
 	}
 }
