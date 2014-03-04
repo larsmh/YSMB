ccf949b3960d12c27e8d7802d2eafd3c4ed732f6
tofo145 <tore.bjorlo@gmail.com>
2014-03-04 13:24:05 +0100

    more ship class

--- src/com/tdt4240/yousunkmybattleship/Ship.java
+++ src/com/tdt4240/yousunkmybattleship/Ship.java
@@ -1,16 +1,26 @@
 package com.tdt4240.yousunkmybattleship;
 
 public class Ship {
+	private final String VERTICAL = "VERTICAL";
+	private final String HORIZONTAL = "HORIZONTAL";
+	private String direction;
+	
 	private int size;
 	private int[] ship;
-	
-	private String direction;
+	private int posX, posY;
 	
 	private boolean shipSunk;
 	
 	public Ship(int size, int[] ship) {
 		this.size = size;
+		
+		/*this.ship = new int[size];
+		for (int i = 0; i < size; i++) {
+			this.ship[i] = ship[i];
+		}*/
+		
 		this.ship = ship;
+		
 		this.shipSunk = false;
 	}
 	
@@ -18,10 +28,30 @@ public class Ship {
 		return this.shipSunk;
 	}
 	
+	public void setVertical() {
+		this.direction = VERTICAL;
+	}
+	
+	public void setHorizontal() {
+		this.direction = HORIZONTAL;
+	}
+	
 	public boolean isVertical() {
-		if (direction == "VERTICAL") {
+		if (this.direction == VERTICAL) {
 			return true;
 		}
 		return false;
+	}
+	
+	public void placeShip(int posX, int posY, String direction) {
+		
+	}
+	
+	public int getPosX() {
+		return this.posX;
+	}
+	
+	public int getPosY() {
+		return this.posY;
 	}
 }
