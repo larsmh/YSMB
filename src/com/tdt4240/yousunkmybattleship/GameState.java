9e25d4e8a22dbe9a0f3fc5563ea38c0bdfbb482e
Chloebrz <chloe.brouzes@hotmail.fr>
2014-03-04 13:18:13 +0100

    add methods to PlayerState

--- src/com/tdt4240/yousunkmybattleship/GameState.java
+++ src/com/tdt4240/yousunkmybattleship/GameState.java
@@ -1,7 +1,26 @@
 package com.tdt4240.yousunkmybattleship;
 
+import android.graphics.Canvas;
+import android.view.MotionEvent;
 import sheep.game.State;
+import sheep.input.TouchListener;
 
-public class GameState extends State {
+public class GameState extends State implements TouchListener {
+
+	public GameState() {
+
+	}
+
+	public void draw(Canvas canvas) {
+
+	}
+
+	public void update(float dt) {
+
+	}
+
+	public boolean onTouchMove(MotionEvent event) {
+		return false;
+	}
 
 }
