577b701fb725c82afaac83c76152214158d758e4
Chloebrz <chloe.brouzes@hotmail.fr>
2014-03-11 11:44:35 +0100

    GameOverState display winner

--- src/com/tdt4240/yousunkmybattleship/Constants.java
+++ src/com/tdt4240/yousunkmybattleship/Constants.java
@@ -5,29 +5,28 @@ import sheep.game.Game;
 
 public class Constants {
 	public static Game game;
-	public Player p1, p2;
-	
+	public static Player p1, p2;
+
 	/* Window sizes */
-	public static float WINDOW_WIDTH  = 0;
+	public static float WINDOW_WIDTH = 0;
 	public static float WINDOW_HEIGHT = 0;
-	
+
 	/* Grid properties */
 	public static int GRID_WIDTH = 10;
 	public static int GRID_HEIGHT = 10;
 	public static float TILE_SIZE = 108;
 	public static float START_OF_GRID = 840;
-	
+
 	public static int NUMBER_SHIPS = 5;
-	
+
 	public static Paint[] paint;
-	
+
 	public enum DirectionType {
 		HORIZONTAL, VERTICAL;
 	}
-	
 
-	public Player getOther(Player p){
-		if(p==p1)
+	public Player getOther(Player p) {
+		if (p == p1)
 			return p2;
 		else
 			return p1;
